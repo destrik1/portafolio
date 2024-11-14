4bce0932cfcfa663321e26e00fd9d8bd858f91eb
Destrik1~ <este.martinezs@.duocuc.cl>
2024-11-14 19:55:07 -0300

    cambios2

--- src/Components/AudioPlayer.tsx
+++ src/Components/AudioPlayer.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import songFile from "../assets/Song/Fuera.mp3";
 import songFile1 from "../assets/Song/Amarella.mp3";
 import songFile2 from "../assets/Song/ElMomento.mp3";
 import songFile3 from "../assets/Song/EsaDroga.mp3";
@@ -17,61 +16,68 @@ interface Song {
 
 // List of songs with unique IDs, artists, and URLs
 const songs: Song[] = [
-  {
-    id: 1,
-    title: "y si no fuera",
-    artist: "Artista 1",
-    audioSrc: songFile,
-    spotifyUrl: "https://open.spotify.com/track/spotify-id-1",
-    youtubeUrl: "https://www.youtube.com/watch?v=youtube-id-1",
-  },
+
   {
     id: 2,
     title: "Amarella",
-    artist: "Artista 2",
+    artist: "NR x Matita x PAYA x FRESKOBEATS",
     audioSrc: songFile1,
-    spotifyUrl: "https://open.spotify.com/track/spotify-id-2",
-    youtubeUrl: "https://www.youtube.com/watch?v=youtube-id-2",
+    spotifyUrl:
+      "https://open.spotify.com/intl-es/track/3ImFUPNS1fQFEbcGwiYzhB?si=ebd46446134b4fa2",
+    youtubeUrl: "https://www.youtube.com/watch?v=3PEsPqIOlzU",
   },
   {
     id: 3,
     title: "El Momento",
-    artist: "Artista 3",
+    artist: "PAYA x DJ LUVID",
     audioSrc: songFile2,
-    spotifyUrl: "https://open.spotify.com/track/spotify-id-3",
-    youtubeUrl: "https://www.youtube.com/watch?v=youtube-id-3",
+    spotifyUrl:
+      "https://open.spotify.com/intl-es/track/07sMYGHaCgtGImSLMN2lhg?si=d1eedb0b6b654521",
+    youtubeUrl: "https://youtu.be/NVuc4C25WmY?si=zUdEg2BUfPUZhEOl",
   },
   {
     id: 4,
     title: "Esa Droga",
-    artist: "Artista 4",
+    artist: "PAYA x DJ LUVID",
     audioSrc: songFile3,
-    spotifyUrl: "https://open.spotify.com/track/spotify-id-4",
-    youtubeUrl: "https://www.youtube.com/watch?v=youtube-id-4",
+    spotifyUrl:
+      "https://open.spotify.com/intl-es/track/0wwDRmD9Ak75lcl8HjkNfq?si=fc5f194ce8e342ec",
+    youtubeUrl: "https://youtu.be/Z17LOZi4tCw?si=U1gA8gmkL4PTCd3j",
   },
   {
     id: 5,
     title: "Pegao Pegao",
-    artist: "Artista 5",
+    artist: "PAYA x DJ LUVID Ft Matita, Riilax, Demcy",
     audioSrc: songFile4,
     spotifyUrl: "https://open.spotify.com/track/spotify-id-5",
-    youtubeUrl: "https://www.youtube.com/watch?v=youtube-id-5",
+    youtubeUrl: "https://youtu.be/sHl_OUSFI9I?si=GqzOBmJDLhGcMHhT",
   },
 ];
 
+// Audio player component
 const AudioPlayer = ({ currentSong }: { currentSong: Song | null }) => {
   return (
-    <div className="text-center m-10 h-10">
+    <div className="text-center m-10 h-10 bg">
       {currentSong ? (
-        <audio key={currentSong.id} controls>
-          <source src={currentSong.audioSrc} type="audio/mpeg" />
-          Tu navegador no soporta el elemento de audio.
-        </audio>
+        <div className="flex justify-center">
+          <audio
+            key={currentSong.id}
+            controls
+            className="w-3/4" // Hacer la barra más larga (75% del contenedor)
+            style={{
+              backgroundColor: "#4CAF50", // Cambiar el color de la barra de reproducción (puedes ajustarlo a tu color)
+              borderRadius: "10px",
+            }}
+          >
+            <source src={currentSong.audioSrc} type="audio/mpeg" />
+            Tu navegador no soporta el elemento de audio.
+          </audio>
+        </div>
       ) : (
         <p>Selecciona una canción para reproducir</p>
       )}
       {currentSong && (
-        <p>
+        <p className="text-white mt-2">
           {currentSong.title} - {currentSong.artist}
         </p>
       )}
@@ -101,8 +107,10 @@ const Playlist = () => {
 
   return (
     <div className="flex justify-center p-7">
-      <div className="w-full max-w-4xl border shadow-xl rounded-lg p-8">
-        <h2 className="text-center text-xl font-semibold mb-6">Playlist</h2>
+      <div className="w-full max-w-4xl border shadow-xl rounded-lg p-8 bg-gray-800">
+        <h2 className="text-center text-4xl font-semibold mb-6 text-white">
+          Playlist
+        </h2>
         <AudioPlayer currentSong={currentSong} />
         <button
           onClick={playNextSong}
@@ -110,9 +118,9 @@ const Playlist = () => {
         >
           Cambiar canción
         </button>
-        <table className="w-full table-auto">
+        <table className="w-full table-auto text-white">
           <thead>
-            <tr className="bg-gray-100 text-gray-600">
+            <tr className="bg-gray-700 text-xl">
               <th className="p-3 text-center font-medium">Canción</th>
               <th className="p-3 text-center font-medium">Artista</th>
               <th className="p-3 text-center font-medium">Acciones</th>
@@ -121,7 +129,7 @@ const Playlist = () => {
           </thead>
           <tbody>
             {songs.map((song, index) => (
-              <tr key={song.id} className="hover:bg-gray-200 cursor-pointer">
+              <tr key={song.id} className="hover:bg-gray-600 cursor-pointer">
                 <td className="border p-3 text-center">{song.title}</td>
                 <td className="border p-3 text-center">{song.artist}</td>
                 <td className="border p-3 text-center flex justify-center gap-2">
